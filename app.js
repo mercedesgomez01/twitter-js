792b2ee31aff6d89ad4a270d035fd6336d06b5be
mercedesgomez01 <mercedesgomez01@gmail.com>
2017-04-19 16:01:19 -0400

    added port 3000

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
+
+
+app.get('/', function (req, res) {
+  res.send('Welcome!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
