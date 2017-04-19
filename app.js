e9d963ab4ea41aff116cb5c20363cc70c14eccc4
mercedesgomez01 <mercedesgomez01@gmail.com>
2017-04-19 16:14:27 -0400

    Added chalk

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
+const chalk = require('chalk')
 
+app.use(function (req, res, next) {
+  console.log('Time:', Date.now())
+  next()
+})
 
 app.get('/', function (req, res) {
   res.send('Welcome!')
@@ -8,4 +13,6 @@ app.get('/', function (req, res) {
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
+  var stringz = 'Example app listening on port 3000!'
+  console.log(chalk.red(stringz))
 })
